62886998fcb3d8294c1988f23cb63d11c9578064
Ignas Biekša <ignasbieksa@gmail.com>
2021-12-19 19:33:23 +0200

    added collision

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const state = {
   spaceshipW: 50,
   enemies: [],
   enemyW: 60,
-  enemyNum: 12
+  enemyNum: 18,
 };
 
 function setPosition($element, x, y) {
@@ -61,10 +61,19 @@ function createPew($container, x, y) {
   setPosition($pew, x, y);
 }
 
-function deletePew(pewpew, pew, $pew){
-    const index = pewpew.indexOf(pew);
-    pewpew.splice(index, 1);
-    $container.removeChild($pew);
+function deletePew(pewpew, pew, $pew) {
+  const index = pewpew.indexOf(pew);
+  pewpew.splice(index, 1);
+  $container.removeChild($pew);
+}
+
+function collision(rect1, rect2) {
+  return !(
+    rect2.left > rect1.right ||
+    rect2.right < rect1.left ||
+    rect2.top > rect1.bottom ||
+    rect2.bottom < rect1.top
+  );
 }
 
 function updatePew() {
@@ -72,58 +81,75 @@ function updatePew() {
   for (let i = 0; i < pewpew.length; i++) {
     const pew = pewpew[i];
     pew.y -= 2;
-    if(pew.y < 0) {
-        deletePew(pewpew, pew, pew.$pew);
+    if (pew.y < 0) {
+      deletePew(pewpew, pew, pew.$pew);
     }
     setPosition(pew.$pew, pew.x, pew.y);
+    const pewRect = pew.$pew.getBoundingClientRect();
+    const enemies = state.enemies;
+    for (let j = 0; j < enemies.length; j++) {
+      const enemy = enemies[j];
+      const enemyRect = enemy.$enemy.getBoundingClientRect();
+      if (collision(enemyRect, pewRect)) {
+        deletePew(pewpew, pew, pew.$pew);
+        const index = enemies.indexOf(enemy);
+        enemies.splice(index, 1);
+        $container.removeChild(enemy.$enemy);
+      }
+    }
   }
 }
 
 function updatePlayer() {
   if (state.moveLeft) {
     state.x -= 3;
-  } if (state.moveRight) {
+  }
+  if (state.moveRight) {
     state.x += 3;
-  } if (state.shoot && state.cooldown == 0) {
+  }
+  if (state.shoot && state.cooldown == 0) {
     createPew($container, state.x - state.spaceshipW / 2, state.y);
     state.cooldown = 30;
   }
   const $player = document.querySelector(".player");
   setPosition($player, bound(state.x), state.y);
-  if(state.cooldown > 0){
-      state.cooldown -= 0.5;
+  if (state.cooldown > 0) {
+    state.cooldown -= 0.5;
   }
 }
 
 function createEnemy($container, x, y) {
-    const $enemy = document.createElement("img");
-    $enemy.src = "assets/alien.png";
-    $enemy.className = "enemy";
-    $container.appendChild($enemy);
-    const enemy = {x, y, $enemy};
-    state.enemies.push(enemy);
-    setSize($enemy, state.enemyW);
-    setPosition($enemy, x, y);
+  const $enemy = document.createElement("img");
+  $enemy.src = "assets/alien.png";
+  $enemy.className = "enemy";
+  $container.appendChild($enemy);
+  const enemy = { x, y, $enemy };
+  state.enemies.push(enemy);
+  setSize($enemy, state.enemyW);
+  setPosition($enemy, x, y);
 }
 
 function updateEnemy() {
-    const dx = Math.sin(Date.now()/1000)*50+80;
-    const enemies = state.enemies;
-    for(let i = 0; i<enemies.length; i++) {
-        const enemy = enemies[i];
-        var a = enemy.x + dx;
-        var b = enemy.y-50;
-        setPosition(enemy.$enemy, a, b);
-    }
+  const dx = Math.sin(Date.now() / 1000) * 90 + 80;
+  const enemies = state.enemies;
+  for (let i = 0; i < enemies.length; i++) {
+    const enemy = enemies[i];
+    var a = enemy.x + dx;
+    var b = enemy.y - 50;
+    setPosition(enemy.$enemy, a, b);
+  }
 }
 
-function createEnemies($container){
-    for(let i = 0; i <= state.enemyNum/2; i++) {
-        createEnemy($container, i*80, 100);
-    }
-    for(let i = 0; i <= state.enemyNum/2; i++) {
-        createEnemy($container, i*80, 180);
-    }
+function createEnemies($container) {
+  for (let i = 0; i <= state.enemyNum / 3; i++) {
+    createEnemy($container, i * 80, 100);
+  }
+  for (let i = 0; i <= state.enemyNum / 3; i++) {
+    createEnemy($container, i * 80, 180);
+  }
+  for (let i = 0; i <= state.enemyNum / 3; i++) {
+    createEnemy($container, i * 80, 260);
+  }
 }
 
 function keyPress(event) {
